b4a93500bd0bddfbe4020f6d10bda16d1331df68
denevB <95876310+denevB@users.noreply.github.com>
2023-11-23 23:00:35 +0200

    origin

--- src/lib/components/page/product/AddToCart.svelte
+++ src/lib/components/page/product/AddToCart.svelte
@@ -6,15 +6,76 @@
     } from "../../../../__generated__/graphql.js";
     import ProductItem from "../product/Product.svelte";
     import { page } from "$app/stores";
-    import { PUBLIC_IMG_URL } from "$env/static/public";
+
     export let pageData: ShopPage;
     export let product: Product
     export let wine: ComponentProductProductName[]
+
+    let volume: number = 0
+    let quantity: number = 1
+    $:quantity
+
+    $: wine.forEach((item)=>{
+        if(item.quantity){
+            volume += Number(item.volume) * Number(item.quantity)
+        } else {
+            volume += Number(item.volume)
+        }
+    })
+
+    function adjustQunatity(direction: "add" | "substract"){
+        if(direction === "add"){
+            quantity += 1
+        }
+        if(direction === "substract" && quantity > 1) {
+            quantity -= 1
+        }
+    }
+
+
+
 </script>
 
-<div class=" w-full h-96 border-l border-brown sticky top-32">
-    <ProductItem 
-        packageTitle={product.packageTitle}
-        wine={wine}
-    />
+<div class=" w-full border-l border-brown sticky top-32">
+    <div class="p-10 flex flex-col space-y-6">
+        <div>
+            <p class="text-brown font-sansy uppercase">{product.productType?.data?.attributes?.productType}</p>
+            {#if wine.length > 1 }
+                <div class="mt-2">
+                    {#each wine as el }
+                        <p class="text-xs font-serif italic text-gray">{el.quantity} x {el.vina?.data?.attributes?.name}</p>
+                    {/each}
+                </div>
+            {/if}
+        </div>
+        <div>
+            <p class="font-serif text-gray text-2xl">{Number(product.regularPrice).toFixed(2)} лв.</p>
+            <p class="font-serif text-brown text-sm">{(Number(product.regularPrice)/volume).toFixed(2)} лв./л</p>
+        </div>
+        <div class="flex space-x-6">
+            <div class="border border-brown justify-between w-16 h-10 rounded-md flex">
+                <div class="flex flex-col items-center justify-center w-full">
+                    <p class="text-brown font-serif">{quantity}</p>
+                </div>
+                <div class="flex border-l border-brown flex-col w-8">
+                    <button
+                        on:click={()=>{
+                            adjustQunatity("add")
+                        }}
+                        class="h-full items-center flex justify-center relative"><span class="w-2 absolute h-0.5 rounded-full bg-brown"></span><span class="w-2 absolute rotate-90 h-0.5 rounded-full bg-brown"></span></button>
+                    <div class="h-0.5 w-full bg-brown"></div>
+                    <button
+                        on:click={()=>{
+                            adjustQunatity("substract")
+                        }}
+                        class="h-full items-center flex justify-center"><span class="w-2 h-0.5 rounded-full bg-brown"></span></button>
+                </div>
+            </div>
+            <button class="uppercase font-sansy text-white bg-brown bg-opacity-70 hover:bg-opacity-100 transition-all duration-300 px-10">Добави</button>
+        </div>
+        <ProductItem 
+            packageTitle={product.packageTitle}
+            wine={wine}
+        />
+    </div>
 </div>
--- src/lib/components/page/product/Product.svelte
+++ src/lib/components/page/product/Product.svelte
@@ -5,12 +5,12 @@
     export let wine: ComponentProductProductName[]
 </script>
 
-<article class="">
+<article class="border-y border-brown border-opacity-40">
         <div class="w-full flex flex-col items-center pt-14 pb-10 px-5 relative h-full ">
             {#if wine.length == 1 }
                 {#each wine as el }
                     <div class="">
-                        <img class="pb-4 h-60" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                        <img class="pb-4 h-52" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
                     </div>
                     <h3 class="text-brown italic font-serif text-lg text-center">{el.vina?.data?.attributes?.name}</h3>
                 {/each}
@@ -18,12 +18,12 @@
 
             <div class="w-full flex flex-col items-center">
                 {#if wine.length == 2 }
-                    <div class="grid grid-cols-2 h-52 w-full">
+                    <div class="grid grid-cols-2 h-48 w-full">
                         {#each wine as el }
                             <div class="relativ w-full flex justify-center">
-                                <img class="class absolute max-w-[70px] -translate-x-8 scale-95 opacity-80 object-contain h-52" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
-                                <img class="class absolute opacity-80 translate-x-8 scale-95 max-w-[70px] object-contain h-52" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
-                                <img class="class absolute max-w-[70px] object-contain h-52" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                                <img class="class absolute max-w-[70px] -translate-x-6 scale-95 opacity-80 object-contain h-40" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                                <img class="class absolute opacity-80 translate-x-6 scale-95 max-w-[70px] object-contain h-40" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                                <img class="class absolute max-w-[70px] object-contain h-40" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
                             </div>
                         {/each}
                     </div>
@@ -33,11 +33,11 @@
 
             <div class="w-full flex flex-col items-center">
                 {#if wine.length == 3 }
-                    <div class="grid grid-cols-3 h-52 w-full pb-4">
+                    <div class="grid grid-cols-3 h-48 w-full pb-4">
                         {#each wine as el }
                             <div class="relative flex justify-center -translate-x-2">
-                                <img class="class absolute translate-x-4 opacity-80 scale-95 h-52" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
-                                <img class="class absolute h-52" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                                <img class="class absolute translate-x-4 opacity-80 scale-95 h-40" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                                <img class="class absolute h-40" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
                             </div>
                         {/each}
                     </div>
@@ -48,10 +48,10 @@
 
             <div class="w-full flex flex-col items-center">
                 {#if wine.length > 3 }
-                    <div class="grid grid-cols-3 h-52 w-full pb-4 -translate-x-4">
+                    <div class="grid grid-cols-3 h-48 w-full pb-4 -translate-x-4">
                         {#each wine as el, i }
                         <div class="flex flex-col items-center">
-                            <img class="class h-52 {i > 2 ? " -translate-y-full translate-x-8 scale-95 z-0 opacity-90" : "scale-100 z-10"}" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
+                            <img class="class h-40 {i > 2 ? " -translate-y-full translate-x-6 scale-95 z-0 opacity-90" : "scale-100 z-10"}" src="{PUBLIC_IMG_URL + el.vina?.data?.attributes?.image?.data?.attributes?.url+"?format=webp"}" alt="{el.vina?.data?.attributes?.image?.data?.attributes?.alternativeText}">
                         </div>
                         {/each}
                     </div>
