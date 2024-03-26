bcc6b5eded02db727b7d9f84618291229d1bf00f
denevB <95876310+denevB@users.noreply.github.com>
2024-03-26 15:26:48 +0200

    origin

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,44 +1,6 @@
-import { error, type Handle, type RequestEvent } from "@sveltejs/kit";
-import { allowedOrigins } from "$lib/config/allowedOrigins";
-
-const csrf = (
-    event: RequestEvent,
-    allowedOrigins: string[],
-) => {
-    const { request, url } = event;
-
-    const forbidden =
-        isFormContentType(request) &&
-        (request.method === "POST" ||
-            request.method === "PUT" ||
-            request.method === "PATCH" ||
-            request.method === "DELETE") &&
-        !allowedOrigins.includes(request.headers.get("origin") || "");
-
-    if (forbidden) {
-        error(403, `Cross-site ${request.method} form submissions are forbidden`);
-    }
-};
-
-function isContentType(request: Request, ...types: string[]) {
-    const type = request.headers.get("content-type")?.split(";", 1)[0].trim() ?? "";
-    return types.includes(type.toLowerCase());
-}
-function isFormContentType(request: Request) {
-    // These content types must be protected against CSRF
-    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/enctype
-    return isContentType(
-        request,
-        "application/x-www-form-urlencoded",
-        "multipart/form-data",
-        "text/plain",
-    );
-}
-
 export async function handle({ event, resolve }) {
     let locale = event.params.lang ? event.params.lang : "bg"
-    csrf(event, allowedOrigins);
-
+    
     const response = await resolve(event, {
         transformPageChunk: ({html}) => html.replace("%lang%", locale),
     })
--- src/lib/config/allowedOrigins.js
+++
@@ -1,1 +0,0 @@
-export const allowedOrigins = ["https://www.santa-sarah.com", "https://santa-sarah.com", "http://localhost:5173"];
