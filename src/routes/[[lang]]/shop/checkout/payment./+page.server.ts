1267c4c7d80fd0105a6b0433b22b0dc53a8842ce
denevB <95876310+denevB@users.noreply.github.com>
2024-04-29 23:52:58 +0300

    origin

--- src/routes/[[lang]]/shop/checkout/payment./+page.server.ts
+++ src/routes/[[lang]]/shop/checkout/payment./+page.server.ts
@@ -1,7 +1,8 @@
+import { json } from '@sveltejs/kit'
 
 /** @type {import('@sveltejs/kit').Load} */
 export const load = async ({ url, params, request }) => {
  console.log(request)
 
- return request
+ return request.body
 }
